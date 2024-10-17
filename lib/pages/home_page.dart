ecf48f7338f4cb586745e00fa84460728497f235
Daniyeremi12345 <202100352G@utea.edu.pe>
2024-10-16 22:41:47 -0500

    botones prueba

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,88 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+
+
+class HomePage extends StatelessWidget {
+
+CollectionReference tasksReference = FirebaseFirestore.instance.collection('tasks');
+
+
+@override
+Widget build(BuildContext context) {
+return Scaffold(
+appBar: AppBar(
+title: Text("Firebase danii"),
+),
+
+body: Center(
+  child: Column(
+    mainAxisAlignment: MainAxisAlignment.center ,
+    children: [
+      ElevatedButton(
+        
+        onPressed: (){
+          tasksReference.get().then((QuerySnapshot value){
+            
+            QuerySnapshot collection = value;
+            collection.docs.forEach((QueryDocumentSnapshot element){
+              Map <String, dynamic> myMap=element.data() as Map<String, dynamic>;
+              print(myMap["title"]);
+            }
+            );
+          });
+        }, 
+
+        child: Text("Obtener la data")
+      ),
+    
+      ElevatedButton(onPressed: (){
+
+        tasksReference .add(
+          {
+            "title":"Ir a comprar mangas puercos 2",
+            "description": "comprar coca cola"  
+
+          },
+          ).then(( DocumentReference value){
+            print(value.id);
+          }).catchError((error){
+            print("Ocurrio un error en el registro");
+          }).whenComplete((){
+            print("El registro ah terminado ");
+          });
+
+      },
+       child: Text("Agregar documento",
+       
+       ),
+       ),
+    
+      ElevatedButton(onPressed: (){
+        tasksReference
+        .doc("hx8sMt3tIjAZyIdVm5FQ")
+        .update(
+          {
+            "title": "Ir de paseo ",
+            "description":"Tenemos que salir muy temprano",
+          },
+          )
+        .catchError(
+          (error){
+            print(error);
+          }
+          ).whenComplete((){
+              print("Actualizacion terminada");
+          },
+          ) ;
+      },       
+      child: Text("Actualizar documento",
+      ),
+      ),
+    ],
+  ),
+),
+
+);
+}
+}
